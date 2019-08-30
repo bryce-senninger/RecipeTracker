dbe2a637ecb3c964eded3f111b704b26bae58649
Bryce <bryce.senninger@gmail.com>
2019-08-30 12:22:45 -0600

    finished styles, awaiting heroku deploy

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO recipes (recipe_name, tried) VALUES ('Alfredo Shrimp', false);
 INSERT INTO recipes (recipe_name, tried) VALUES ('Chicken Paella', false);
 INSERT INTO recipes (recipe_name, tried) VALUES ('Steak Fajitas', false);
